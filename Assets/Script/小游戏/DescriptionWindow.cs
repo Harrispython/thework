cc8173dc566f3e45e4ef536c329cc8fe3749d968
Smallsign <1131431962@qq.com>
2025-03-19 15:14:39 +0800

    同步

--- Assets/Script/小游戏/DescriptionWindow.cs
+++ Assets/Script/小游戏/DescriptionWindow.cs
@@ -10,6 +10,8 @@ public class DescriptionWindow : MonoBehaviour
 
     public Action<ItemMessage> OnWindowSet; // 事件委托
     public static DescriptionWindow instance;
+    public ItemMessage message;
+
     private void Start()
     {
         // 订阅事件
@@ -25,7 +27,9 @@ public class DescriptionWindow : MonoBehaviour
     }
     public void SetWindow(ItemMessage Message)
     {
+        message = Message;
         this.transform.Find("名字").GetComponent<TextMeshProUGUI>().text = $"名字:{Message.message.ItemName}";
+        this.transform.Find("重量").GetChild(0).GetComponent<TMP_InputField>().text = "";
         
     }
 
